e63a5e70bc1f552f47db33d900f3177d141e666f
Mahmudul Hasan Sabbir <32410301+SAA33IR@users.noreply.github.com>
2022-12-03 23:14:23 +0600

    Update SDKImporter.cs

--- SDKImporter.cs
+++ SDKImporter.cs
@@ -1,3 +1,5 @@
+//Download and Import External Dependency Manager, Facebook, AppsFlyer, GameAnalytics
+
 #if UNITY_EDITOR
 using System.IO;
 using UnityEngine;
@@ -6,41 +8,31 @@ using System.Net.Http;
 
 public class SDKImporter
 {
-    private const string url_edm = "https://raw.githubusercontent.com/googlesamples/unity-jar-resolver/master/external-dependency-manager-latest.unitypackage";
-    private const string url_af = "https://raw.githubusercontent.com/AppsFlyerSDK/appsflyer-unity-plugin/master/appsflyer-unity-plugin-6.8.5.unitypackage";
+    private const string url_dm = "https://github.com/googlesamples/unity-jar-resolver/raw/master/external-dependency-manager-latest.unitypackage";
+    private const string url_kv = "https://github.com/arifurrahman-devtools/ptsd-kit/releases/download/v0.1.1/PTSDKit_0.1.1.unitypackage";
+    private const string url_fb = "https://github.com/SAA33IR/Stable-SDKs/raw/main/facebook.unitypackage";
+    private const string url_af = "https://github.com/AppsFlyerSDK/appsflyer-unity-plugin/raw/master/appsflyer-unity-plugin-6.8.5.unitypackage";
     private const string url_ga = "https://download.gameanalytics.com/unity/GA_SDK_UNITY.unitypackage";
 
-    [MenuItem("Tools/Import SDK/AppsFlyer")]
-    private static async void AF()
-    {
-        string fileName = "AppsFlyer.unitypackage";
-        var client = new HttpClient();
-        var response = await client.GetAsync(url_af);
-        var contents = await response.Content.ReadAsByteArrayAsync();
-        File.WriteAllBytes(fileName, contents);
-        AssetDatabase.ImportPackage(fileName, true);
-        File.Delete(fileName);
-    }
+    [MenuItem("Tools/Import SDK/External Dependency Manager", false, 0)]
+    private static void EDM() => GetUnityPackage(url_dm, "ExternalDependencyManager.unitypackage");
 
+    [MenuItem("Tools/Import SDK/PTSD Kit", false, 20)]
+    private static void PTSDKit() => GetUnityPackage(url_kv, "PTSDKit.unitypackage");
 
-    [MenuItem("Tools/Import SDK/External Dependency Manager")]
-    private static async void EDM()
-    {
-        string fileName = "ExternalDependencyManager.unitypackage";
-        var client = new HttpClient();
-        var response = await client.GetAsync(url_edm);
-        var contents = await response.Content.ReadAsByteArrayAsync();
-        File.WriteAllBytes(fileName, contents);
-        AssetDatabase.ImportPackage(fileName, true);
-        File.Delete(fileName);
-    }
+    [MenuItem("Tools/Import SDK/Facebook", false, 40)]
+    private static void FB() => GetUnityPackage(url_fb, "facebook.unitypackage");
 
-    [MenuItem("Tools/Import SDK/GameAnalytics")]
-    private static async void GA()
+    [MenuItem("Tools/Import SDK/AppsFlyer", false, 41)]
+    private static void AF() => GetUnityPackage(url_af, "AppsFlyer.unitypackage");
+
+    [MenuItem("Tools/Import SDK/GameAnalytics",false, 42)]
+    private static void GA() => GetUnityPackage(url_ga, "GameAnalytics.unitypackage");
+
+    private static async void GetUnityPackage(string url, string fileName)
     {
-        string fileName = "GameAnalytics.unitypackage";
         var client = new HttpClient();
-        var response = await client.GetAsync(url_ga);
+        var response = await client.GetAsync(url);
         var contents = await response.Content.ReadAsByteArrayAsync();
         File.WriteAllBytes(fileName, contents);
         AssetDatabase.ImportPackage(fileName, true);
