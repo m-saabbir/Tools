10e727ad9afa65e861e45f02a49b06817529d019
Mahmudul Hasan Sabbir <32410301+SAA33IR@users.noreply.github.com>
2022-12-03 20:22:47 +0600

    Update SDKImporter.cs

--- SDKImporter.cs
+++ SDKImporter.cs
@@ -7,7 +7,21 @@ using System.Net.Http;
 public class SDKImporter
 {
     private const string url_edm = "https://raw.githubusercontent.com/googlesamples/unity-jar-resolver/master/external-dependency-manager-latest.unitypackage";
+    private const string url_af = "https://raw.githubusercontent.com/AppsFlyerSDK/appsflyer-unity-plugin/master/appsflyer-unity-plugin-6.8.5.unitypackage";
     private const string url_ga = "https://download.gameanalytics.com/unity/GA_SDK_UNITY.unitypackage";
+
+    [MenuItem("Tools/Import SDK/AppsFlyer")]
+    private static async void AF()
+    {
+        string fileName = "AppsFlyer.unitypackage";
+        var client = new HttpClient();
+        var response = await client.GetAsync(url_af);
+        var contents = await response.Content.ReadAsByteArrayAsync();
+        File.WriteAllBytes(fileName, contents);
+        AssetDatabase.ImportPackage(fileName, true);
+        File.Delete(fileName);
+    }
+
 
     [MenuItem("Tools/Import SDK/External Dependency Manager")]
     private static async void EDM()
