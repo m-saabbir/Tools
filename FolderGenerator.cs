e5fea026b90692efdd6f018ef0d4aec828d71714
Mahmudul Hasan Sabbir <32410301+SAA33IR@users.noreply.github.com>
2022-12-03 21:01:06 +0600

    Add files via upload

---
+++ FolderGenerator.cs
@@ -0,0 +1,44 @@
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class FolderGenerator
+{
+    [MenuItem("Tools/Generate Folders")]
+    private static void GenerateFolders()
+    {
+        List<string> folders = new List<string> { "_Arts", "_Prefabs", "_Scenes", "_Scripts", "_UI", "Externals" };
+        
+        foreach (string folder in folders)
+        {
+            if (!Directory.Exists("Assets/" + folder))
+            {
+                Directory.CreateDirectory("Assets/" + folder);
+            }
+        }
+
+        List<string> artsFolders = new List<string>{"Animations", "Materials", "Models", "Particles", "Textures"};
+
+        foreach (string artsFolder in artsFolders)
+        {
+            if (!Directory.Exists("Assets/_Arts/" + artsFolder))
+            {
+                Directory.CreateDirectory("Assets/_Arts/" + artsFolder);
+            }
+        }
+
+        List<string> uiFolders = new List<string> {"Fonts", "Icons", "Sprites"};
+
+        foreach (string uiFolder in uiFolders)
+        {
+            if (!Directory.Exists("Assets/_UI/" + uiFolder))
+            {
+                Directory.CreateDirectory("Assets/_UI/" + uiFolder);
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
+#endif
